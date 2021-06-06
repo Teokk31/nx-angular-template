d432069208a2bbe07b69ed34e8278393de4231ae
Teo Khai Kiong <xcteo31@gmail.com>
2021-06-07 01:57:01 +0800

    feat: generate feature library

--- jest.config.js
+++ jest.config.js
@@ -5,5 +5,6 @@ module.exports = {
     '<rootDir>/libs/root/data-access',
     '<rootDir>/libs/github-users/domain',
     '<rootDir>/libs/github-users/data-access',
+    '<rootDir>/libs/github-users/feature-users',
   ],
 };
---
+++ libs/github-users/feature-users/README.md
@@ -0,0 +1,7 @@
+# github-users-feature-users
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test github-users-feature-users` to execute the unit tests.
