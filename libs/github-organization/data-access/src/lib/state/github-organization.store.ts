b40f00480424c659e30c4bf723edc6ff96166fc9
KKTEO <KKTEO@orsted.dk>
2021-06-08 19:05:48 +0800

    feat: puts in the logic to the github organzation component store

--- libs/github-organization/data-access/src/lib/state/github-organization.store.ts
+++ libs/github-organization/data-access/src/lib/state/github-organization.store.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
-import { ComponentStore } from '@ngrx/component-store';
+import { ComponentStore, tapResponse } from '@ngrx/component-store';
 import { GitHubOrganizations } from '@workspace/github-organization/domain';
+import { Observable } from 'rxjs';
+import { mergeMap } from 'rxjs/operators';
+import { GitHubOrganizationService } from './../http/github-organization.service';
 
 interface GitHubOrganizationState {
   readonly organizations: GitHubOrganizations;
@@ -14,7 +17,43 @@ const initialState: GitHubOrganizationState = {
 
 @Injectable()
 export class GitHubOrganizationStore extends ComponentStore<GitHubOrganizationState> {
-  constructor() {
+  organizations$: Observable<GitHubOrganizations> = this.select(
+    (state) => state.organizations
+  );
+  errorMessage$: Observable<string> = this.select(
+    (state) => state.errorMessage
+  );
+
+  constructor(private gitHubOrganizationService: GitHubOrganizationService) {
     super(initialState);
+
+    this.queryOrganizations();
   }
+
+  private queryOrganizations = this.effect<void>((_$) => {
+    return _$.pipe(
+      mergeMap(() =>
+        this.gitHubOrganizationService.getOrganizations().pipe(
+          tapResponse(
+            (response) => this.updateOrganizations(response),
+            (error) => this.updateErrorMessage(error as string)
+          )
+        )
+      )
+    );
+  });
+
+  private updateOrganizations = this.updater<GitHubOrganizations>(
+    (state, response) => ({
+      ...state,
+      organizations: response,
+      errorMessage: '',
+    })
+  );
+
+  private updateErrorMessage = this.updater<string>((state, response) => ({
+    ...state,
+    organizations: [],
+    errorMessage: response,
+  }));
 }
